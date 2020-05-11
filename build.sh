6a568df760e61a50c07146c9b98f70b5da3d6635
Jeff Okamoto <jokamoto@verizonmedia.com>
2020-05-11 10:00:30 -0700

    Build script.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -x
+perl senate.pl 50 > csv/50.csv
+perl senate.pl 55 > csv/55.csv
+perl senate.pl 60 > csv/60.csv
+perl senate.pl 65 > csv/65.csv
+perl senate.pl 70 > csv/70.csv
+perl senate.pl 75 > csv/75.csv
+perl senate.pl 80 > csv/80.csv
